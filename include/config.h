96503ae415b4e99192a2fd8ac5a70fcb75b3299c
Ruben d'Arco <cyclops-github@prof-x.net>
2024-12-26 14:25:49 +0000

    Strip most of the code down

--- include/config.h
+++ include/config.h
@@ -2,8 +2,6 @@
 #include <Arduino.h>
 #include <serialhelper.h>
 #include <lorahelper.h>
-#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
-
 #include <Adafruit_LittleFS.h>
 #include <InternalFileSystem.h>
 using namespace Adafruit_LittleFS_Namespace;
@@ -12,22 +10,12 @@ using namespace Adafruit_LittleFS_Namespace;
 enum ConfigType
 {
     SleepTime0 = 0x10,
-    SleepTime1 = 0x11,
-    SleepTime2 = 0x12,
-    GPSDynamicModel = 0x20,
-    GPSFixTimeout = 0x21,
-    GPSHDOPLimit = 0x22,
-    GPSPowerSaveMode = 0x23,
     LORA_TXPower = 0x40,
     LORA_DataRate = 0x41,
     LORA_ADREnabled = 0x42,
     LORA_RequireConfirmation = 0x43,
     LORA_DevEUI = 0x48,
     LORA_NodeAppKey = 0x49,
-    MOTION_1stThreshold = 0x51,
-    MOTION_2ndThreshold = 0x52,
-    MOTION_1stDuration = 0x53,
-    MOTION_2ndDuration = 0x54,
     Restart = 0xF0,
     ClearConfig = 0xF1,
     SaveConfig = 0xF2
@@ -44,35 +32,13 @@ struct ConfigOption
 
 struct ConfigurationParameters
 {
-    // Sheeptracker 1
-    // uint8_t _loraDevEUI[8] = {0xAC, 0x1F, 0x09, 0xFF, 0xFE, 0x08, 0xDD, 0xB1};
-    // uint8_t _loraNodeAppKey[16] = {0x66, 0x7b, 0x90, 0x71, 0xa1, 0x72, 0x18, 0xd4, 0xcd, 0xb2, 0x13, 0x04, 0x3f, 0xb2, 0x6b, 0x7c};
-
-    // SheepTracker 2
-    // uint8_t _loraDevEUI[8] = {0xAC, 0x1F, 0x09, 0xFF, 0xFE, 0x08, 0xF5, 0x2B};
-    // uint8_t _loraNodeAppKey[16] = {0x4b, 0xbb, 0x43, 0xef, 0x4b, 0xc6, 0x46, 0x22, 0x1b, 0x0d, 0xcb, 0xe0, 0x44, 0x54, 0xb6, 0x1a};
-
-    // SheepTracker 3
-    // uint8_t _loraDevEUI[8] = {0xAC, 0x1F, 0x09, 0xFF, 0xFE, 0x0C, 0xD7, 0x8A};
-    // uint8_t _loraNodeAppKey[16] = {0xde, 0x84, 0xd1, 0xdc, 0x58, 0x7f, 0xf6, 0x8e, 0xe0, 0xd5, 0x31, 0x40, 0xff, 0x76, 0xb2, 0x89};
-
-    // SheepTracker NEW
-    uint8_t _loraDevEUI[8] = {0xAC, 0x1F, 0x09, 0xFF, 0xAA, 0xAA, 0xAA, 0xAA};
+   // New device has AAA values
+    uint8_t _loraDevEUI[8] = {0xAC, 0x1F, 0x10, 0xFF, 0xAA, 0xAA, 0xAA, 0xAA};
     uint8_t _loraNodeAppKey[16] = {0x66, 0x7b, 0x90, 0x71, 0xa1, 0x72, 0x18, 0xd4, 0xcd, 0xb2, 0x13, 0x04, 0x3f, 0xAA, 0xAA, 0xAA};
 
     // Config settings
     uint16_t _sleeptime0 = 300; // in seconds
-    uint16_t _sleeptime1 = 300;
-    uint16_t _sleeptime2 = 300;
-    uint16_t _gnssFixTimeout = 90; // in seconds
-    uint8_t _gnssDynamicModel = dynModel::DYN_MODEL_PEDESTRIAN;
-    uint8_t _gnssPowerSaveMode = SFE_UBLOX_PMS_MODE_INVALID;
-    uint16_t _gnssHDOPLimit = 200;
 
-    uint8_t _motion1stThreshold = 0x00;
-    uint8_t _motion2ndThreshold = 0x00;
-    uint8_t _motion1stDuration = 0x00;
-    uint8_t _motion2ndDuration = 0x00;
     void *_dummy;
 
     int8_t _loraDataRate = DR_2;
@@ -97,13 +63,6 @@ class ConfigHelper
 
 public:
     uint32_t GetSleepTime0InSeconds() { return configvalues._sleeptime0; }
-    uint32_t GetSleepTime1InSeconds() { return configvalues._sleeptime1; }
-    uint32_t GetSleepTime2InSeconds() { return configvalues._sleeptime2; }
-    uint16_t GetGNSSFixTimeoutInSeconds() { return configvalues._gnssFixTimeout; }
-    uint8_t GetGNSSPowerSaveMode() { return configvalues._gnssPowerSaveMode; }
-    // TODO: make this return `dynModel`. Requires a new Setmethod
-    uint8_t GetGNSSDynamicModel() { return configvalues._gnssDynamicModel; }
-    uint16_t GetGNSSHDOPLimit() { return configvalues._gnssHDOPLimit; }
 
     uint8_t GetLoraTXPower() { return configvalues._loraTXPower; }
     uint8_t GetLoraDataRate() { return configvalues._loraDataRate; }
@@ -112,12 +71,6 @@ public:
     uint8_t *GetLoraNodeAppEUI() { return configvalues._loraNodeAppEUI; }
     uint8_t *GetLoraAppKey() { return configvalues._loraNodeAppKey; }
     bool GetLoraRequireConfirmation() { return configvalues._loraRequireConfirmation; }
-
-    uint8_t GetMotion1stDuration() { return configvalues._motion1stDuration; }
-    uint8_t GetMotion2ndDuration() { return configvalues._motion2ndDuration; }
-
-    uint8_t GetMotion1stThreshold() { return configvalues._motion1stThreshold; }
-    uint8_t GetMotion2ndThreshold() { return configvalues._motion2ndThreshold; }
 
     ConfigOption *GetConfigs(size_t *size);
     void SetConfig(uint8_t *arr, uint8_t length);
@@ -146,22 +99,12 @@ private:
 
     ConfigOption configs[20] = {
         {"Sleep time between GPS fixes (in seconds) - no threshold", ConfigType::SleepTime0, sizeof(ConfigurationParameters::_sleeptime0), &configvalues._sleeptime0, ConfigurationParameters::SetUint16},
-        {"Sleep time between GPS fixes (in seconds) - 1st threshold", ConfigType::SleepTime1, sizeof(ConfigurationParameters::_sleeptime1), &configvalues._sleeptime1, ConfigurationParameters::SetUint16},
-        {"Sleep time between GPS fixes (in seconds) - 2nd threshold", ConfigType::SleepTime2, sizeof(ConfigurationParameters::_sleeptime2), &configvalues._sleeptime2, ConfigurationParameters::SetUint16},
-        {"GPS - Fix timeout (in seconds)", ConfigType::GPSFixTimeout, sizeof(ConfigurationParameters::_gnssFixTimeout), &configvalues._gnssFixTimeout, ConfigurationParameters::SetUint16},
-        {"GPS - Dynamic Model", ConfigType::GPSDynamicModel, sizeof(ConfigurationParameters::_gnssDynamicModel), &configvalues._gnssDynamicModel, ConfigurationParameters::SetUint8},
-        {"GPS - Position Accuracy", ConfigType::GPSHDOPLimit, sizeof(ConfigurationParameters::_gnssHDOPLimit), &configvalues._gnssHDOPLimit, ConfigurationParameters::SetUint16},        
-        {"GPS - PowerSave mode", ConfigType::GPSPowerSaveMode, sizeof(ConfigurationParameters::_gnssPowerSaveMode), &configvalues._gnssPowerSaveMode, ConfigurationParameters::SetUint8},
         {"LoraWAN - TX Power", ConfigType::LORA_TXPower, sizeof(ConfigurationParameters::_loraTXPower), &configvalues._loraTXPower, ConfigurationParameters::SetInt8},
         {"LoraWAN - DataRate", ConfigType::LORA_DataRate, sizeof(ConfigurationParameters::_loraDataRate), &configvalues._loraDataRate, ConfigurationParameters::SetInt8},
         {"LoraWAN - ADR Enabled", ConfigType::LORA_ADREnabled, sizeof(ConfigurationParameters::_loraADREnabled), &configvalues._loraADREnabled, ConfigurationParameters::SetBool},
         {"LoraWAN - Require confirmation message", ConfigType::LORA_RequireConfirmation, sizeof(ConfigurationParameters::_loraRequireConfirmation), &configvalues._loraRequireConfirmation, ConfigurationParameters::SetBool},
         {"LoraWAN - Dev EUI", ConfigType::LORA_DevEUI, sizeof(ConfigurationParameters::_loraDevEUI), &configvalues._loraDevEUI, ConfigurationParameters::SetUint8x8},
         {"LoraWAN - Node App key", ConfigType::LORA_NodeAppKey, sizeof(ConfigurationParameters::_loraNodeAppKey), &configvalues._loraNodeAppKey, ConfigurationParameters::SetUint8x16},
-        {"Motion - 1st interrupt duration", ConfigType::MOTION_1stDuration, sizeof(ConfigurationParameters::_motion1stDuration), &configvalues._motion1stDuration, ConfigurationParameters::SetUint8},
-        {"Motion - 2nd interrupt duration", ConfigType::MOTION_2ndDuration, sizeof(ConfigurationParameters::_motion2ndDuration), &configvalues._motion2ndDuration, ConfigurationParameters::SetUint8},
-        {"Motion - 1st interrupt threshold (0 == disabled)", ConfigType::MOTION_1stThreshold, sizeof(ConfigurationParameters::_motion1stThreshold), &configvalues._motion1stThreshold, ConfigurationParameters::SetUint8},
-        {"Motion - 2nd interrupt threshold (0 == disabled)", ConfigType::MOTION_2ndThreshold, sizeof(ConfigurationParameters::_motion2ndThreshold), &configvalues._motion2ndThreshold, ConfigurationParameters::SetUint8},
         {"Restart Device", ConfigType::Restart, sizeof(ConfigurationParameters::_dummy), &configvalues._dummy, ConfigurationParameters::Restart},
         {"Clear Config", ConfigType::ClearConfig, sizeof(ConfigurationParameters::_dummy), &configvalues._dummy, ConfigurationParameters::DoNothing},
         {"Save Config", ConfigType::SaveConfig, sizeof(ConfigurationParameters::_dummy), &configvalues._dummy, ConfigurationParameters::DoNothing}};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <Wire.h>
-#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include "batteryhelper.h"
 #include "lorahelper.h"
 #include "ledhelper.h"
@@ -8,11 +7,9 @@
 #include "main.h"
 #include "config.h"
 #include "version.h"
-#include "motion.h"
 
 ConfigHelper g_configParams;
 SoftwareTimer g_taskWakeupTimer;
-SFE_UBLOX_GNSS g_GNSS;
 uint16_t g_msgcount = 0;
 
 SemaphoreHandle_t g_taskEvent = NULL;
@@ -69,61 +66,7 @@ void setup()
   // Setup/start the wire that we use for the Sensor.
   Wire.begin();
 
-  // Get all GPS stuff fired up
-  if (g_GNSS.begin(Wire) == false)
-  {
-    SERIAL_LOG("Ublox GPS not detected at default I2C address. Please check wiring. Halting.");
-    LedHelper::BlinkHalt(3);
-  }
 
-  SERIAL_LOG("Found GNSS with Protocol version: %d.%d", g_GNSS.getProtocolVersionHigh(), g_GNSS.getProtocolVersionLow());
-  g_GNSS.factoryReset();
-
-  while (g_GNSS.begin() == false) // Attempt to re-connect
-  {
-    delay(500);
-    SERIAL_LOG("Attempting to re-connect to u-blox GNSS...");
-  }
-  g_GNSS.setDynamicModel((dynModel)g_configParams.GetGNSSDynamicModel());
-  g_GNSS.saveConfigSelective(VAL_CFG_SUBSEC_NAVCONF);
-  g_GNSS.setI2COutput(COM_TYPE_UBX);
-  g_GNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); // Save (only) the communications port settings to flash and BBR
-
-  // We should also disable SBAS and IMES via UBX-CFG-GNSS
-  // We should also turn of time-pulses via UBX-CFG-TP5
-  sfe_ublox_pms_mode_e powerSaveMode = static_cast<sfe_ublox_pms_mode_e>(g_configParams.GetGNSSPowerSaveMode());
-  SERIAL_LOG("PowerSave configuation is %d", powerSaveMode);
-  if (powerSaveMode == SFE_UBLOX_PMS_MODE_INTERVAL)
-  {
-    SERIAL_LOG("Interval powersave mode is not supported.");
-  }
-  else
-  {
-    if (powerSaveMode != SFE_UBLOX_PMS_MODE_INVALID)
-    {
-      if (!g_GNSS.setPowerManagement(powerSaveMode))
-      {
-        SERIAL_LOG("Failed to set PowerManagement");
-        LedHelper::BlinkHalt(4);
-      }
-    }
-  }
-
-  uint8_t powerSave = g_GNSS.getPowerSaveMode();
-  if (powerSave == 255)
-  {
-    SERIAL_LOG("Failed to retrieve powersave mode.");
-    LedHelper::BlinkHalt(4);
-  }
-  SERIAL_LOG("PowerSave move is: %d", powerSave)
-  SERIAL_LOG("GPS is setup, HDOP = %d", g_configParams.GetGNSSHDOPLimit());
-  g_GNSS.saveConfigSelective(VAL_CFG_SUBSEC_RXMCONF); // Store the fact that we want powersave mode
-
-  MotionHelper::InitMotionSensor(
-      g_configParams.GetMotion1stThreshold(),
-      g_configParams.GetMotion2ndThreshold(),
-      g_configParams.GetMotion1stDuration(),
-      g_configParams.GetMotion2ndDuration());
 
 #ifndef LORAWAN_FAKE
   // Lora stuff
@@ -231,38 +174,11 @@ void handleReceivedMessage()
         switch (g_rcvdLoRaData[i])
         {
         case ConfigType::SleepTime0:
-        case ConfigType::SleepTime1:
-        case ConfigType::SleepTime2:
-          SERIAL_LOG("Timers are now %u / %u / %u", g_configParams.GetSleepTime0InSeconds(), g_configParams.GetSleepTime1InSeconds(), g_configParams.GetSleepTime2InSeconds());
+          SERIAL_LOG("Timers are now %u ", g_configParams.GetSleepTime0InSeconds());
           // g_taskWakeupTimer.stop();
           g_taskWakeupTimer.setPeriod(g_configParams.GetSleepTime0InSeconds() * 1000);
           g_taskWakeupTimer.start();
           break;
-        case ConfigType::GPSDynamicModel:
-          SERIAL_LOG("Setting GNSS Dynamic Model to %u", g_configParams.GetGNSSDynamicModel());
-          if (!g_GNSS.setDynamicModel((dynModel)g_configParams.GetGNSSDynamicModel()))
-          {
-            SERIAL_LOG("Failed to set Dynamic Model");
-          }
-          g_GNSS.saveConfigSelective(VAL_CFG_SUBSEC_NAVCONF);
-          break;
-        case ConfigType::GPSPowerSaveMode:
-        {
-          sfe_ublox_pms_mode_e powerSaveModeSomething = static_cast<sfe_ublox_pms_mode_e>(g_configParams.GetGNSSPowerSaveMode());
-          if (powerSaveModeSomething != SFE_UBLOX_PMS_MODE_INTERVAL)
-          {
-            if (!g_GNSS.setPowerManagement(powerSaveModeSomething))
-            {
-              SERIAL_LOG("Failed to set powersave mode");
-            }
-            g_GNSS.saveConfigSelective(VAL_CFG_SUBSEC_RXMCONF);
-          }
-          else
-          {
-            SERIAL_LOG("Not setting powersave mode, due to INTERVAL selected");
-          }
-          break;
-        }
         case ConfigType::LORA_ADREnabled:
         case ConfigType::LORA_DataRate:
           SERIAL_LOG("Setting Lora DataRate to %u and ARD to %d", g_configParams.GetLoraDataRate(), g_configParams.GetLoraADREnabled());
@@ -271,18 +187,6 @@ void handleReceivedMessage()
         case ConfigType::LORA_TXPower:
           SERIAL_LOG("Setting Lora TX Power to %d", g_configParams.GetLoraTXPower());
           LoraHelper::SetTXPower(g_configParams.GetLoraTXPower());
-          break;
-        case ConfigType::MOTION_1stDuration:
-        case ConfigType::MOTION_2ndDuration:
-        case ConfigType::MOTION_1stThreshold:
-        case ConfigType::MOTION_2ndThreshold:
-          SERIAL_LOG("Setting motion sensor to 1/2nd threshold & 1/2nd duration: 0x%02X/0x%02X & 0x%02X/0x%02X",
-                     g_configParams.GetMotion1stThreshold(), g_configParams.GetMotion2ndThreshold(), g_configParams.GetMotion1stDuration(), g_configParams.GetMotion2ndDuration());
-          MotionHelper::InitMotionSensor(
-              g_configParams.GetMotion1stThreshold(),
-              g_configParams.GetMotion2ndThreshold(),
-              g_configParams.GetMotion1stDuration(),
-              g_configParams.GetMotion2ndDuration());
           break;
         }
         i += conf.sizeOfOption; // jump to the next one
@@ -295,127 +199,31 @@ void handleReceivedMessage()
 
 void doPeriodicUpdate()
 {
-  SERIAL_LOG("Doing GPSFix");
+  SERIAL_LOG("doPeriodicUpdate()");
 
   // powersave has a few modes. We use INVALID for managing the power via IO2.
   // We set it HIGH ALWAYS here because you might have come out of a other mode.
   // IO2 is the power to the GPS, so that basically turns it on/off when we need it.
   digitalWrite(WB_IO2, HIGH);
 
-  uint16_t dopLimit = g_configParams.GetGNSSHDOPLimit();
-  uint32_t gnssTimeout = (uint32_t)g_configParams.GetGNSSFixTimeoutInSeconds() * 1000;
-  byte gpsFixType = 0;
-  uint16_t hdop = 0;
+  //TODO: implement code to 
+  
 
-  uint32_t gpsStart = millis();
-  bool gpsPVTStatus = g_GNSS.getPVT();
-  while (!gpsPVTStatus)
-  {
-    SERIAL_LOG("PVT result: %d", gpsPVTStatus);
-    gpsPVTStatus = g_GNSS.getPVT();
-    delay(500);
-  }
-  while (1)
-  {
-    hdop = g_GNSS.getHorizontalDOP();
-    gpsFixType = g_GNSS.getFixType();
-    if (g_GNSS.getGnssFixOk() && hdop <= dopLimit)
-    {
-      SERIAL_LOG("GnnsFixOK && dop <= doplimit");
-      break;
-    }
-    else
-    {
-      SERIAL_LOG("FIXTYPE: %d; HDOP: %d", gpsFixType, hdop);
-      delay(1000);
-    }
-
-    if ((millis() - gpsStart) > gnssTimeout)
-    {
-      LedHelper::BlinkStatus(4);
-      SERIAL_LOG("GNSS fix timeout:  %u > %u", (millis() - gpsStart), gnssTimeout);
-      break;
-    }
-  }
-  uint16_t gpsTimeInSeconds = (uint16_t)((millis() - gpsStart) / 1000);
-  int32_t gpsLat = g_GNSS.getLatitude();
-  int32_t gpsLong = g_GNSS.getLongitude();
-  uint8_t gpsSats = g_GNSS.getSIV();
-  int16_t gpsAltitudeMSL = (g_GNSS.getAltitudeMSL() / 1000);
-
-  SERIAL_LOG("GPS details: GPStime: %us; SATS: %d; FIXTYPE: %d; LAT: %d; LONG: %d; Alt: %d; HDOP: %d\r\n", gpsTimeInSeconds, gpsSats, gpsFixType, gpsLat, gpsLong, gpsAltitudeMSL, hdop);
   uint16_t vbat_mv = BatteryHelper::readVBAT();
-
-  // We are done with the sensors, so we can turn them off
-  sfe_ublox_pms_mode_e powerSaveMode = static_cast<sfe_ublox_pms_mode_e>(g_configParams.GetGNSSPowerSaveMode());
-  if (powerSaveMode == SFE_UBLOX_PMS_MODE_INVALID)
-  {
-    SERIAL_LOG("Setting IO2 LOW");
-    digitalWrite(WB_IO2, LOW);
-  }
 
   // Create the lora message
   memset(g_SendLoraData.buffer, 0, LORAWAN_BUFFER_SIZE);
   int size = 0;
   g_SendLoraData.port = 2;
+  //TODO!!
   g_SendLoraData.buffer[size++] = 0x03;
-  if (MotionHelper::IsMotionEnabled())
-  {
     g_SendLoraData.buffer[size++] = 0x06;
-  }
-  else
-  {
-    g_SendLoraData.buffer[size++] = 0x04;
-  }
 
   g_SendLoraData.buffer[size++] = vbat_mv >> 8;
   g_SendLoraData.buffer[size++] = vbat_mv;
 
   g_SendLoraData.buffer[size++] = g_msgcount >> 8;
   g_SendLoraData.buffer[size++] = g_msgcount;
-
-  g_SendLoraData.buffer[size++] = gpsTimeInSeconds >> 8;
-  g_SendLoraData.buffer[size++] = gpsTimeInSeconds;
-
-  g_SendLoraData.buffer[size++] = gpsSats;
-  g_SendLoraData.buffer[size++] = gpsFixType;
-
-  g_SendLoraData.buffer[size++] = gpsLat >> 24;
-  g_SendLoraData.buffer[size++] = gpsLat >> 16;
-  g_SendLoraData.buffer[size++] = gpsLat >> 8;
-  g_SendLoraData.buffer[size++] = gpsLat;
-
-  g_SendLoraData.buffer[size++] = gpsLong >> 24;
-  g_SendLoraData.buffer[size++] = gpsLong >> 16;
-  g_SendLoraData.buffer[size++] = gpsLong >> 8;
-  g_SendLoraData.buffer[size++] = gpsLong;
-
-  g_SendLoraData.buffer[size++] = gpsAltitudeMSL >> 8;
-  g_SendLoraData.buffer[size++] = gpsAltitudeMSL;
-
-  // TODO: we should add HDOP to this, because we need to know.
-
-  // Add motionresult
-  if (MotionHelper::IsMotionEnabled())
-  {
-    uint8_t motionresult = MotionHelper::GetMotionInterupts();
-    g_SendLoraData.buffer[size++] = motionresult;
-
-    SERIAL_LOG("Motion details: 0x%02X", motionresult)
-    if ((motionresult & 0x10) == 0x10)
-    {
-      g_taskWakeupTimer.setPeriod(g_configParams.GetSleepTime2InSeconds() * 1000);
-    }
-    else if ((motionresult & 0x01) == 0x01)
-    {
-      g_taskWakeupTimer.setPeriod(g_configParams.GetSleepTime1InSeconds() * 1000);
-    }
-    else
-    {
-      g_taskWakeupTimer.setPeriod(g_configParams.GetSleepTime0InSeconds() * 1000);
-    }
-    g_taskWakeupTimer.start();
-  }
 
   g_SendLoraData.buffsize = size;
   SendData();
